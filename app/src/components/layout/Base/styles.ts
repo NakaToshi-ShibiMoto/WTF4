1474bd54b39e58f8af7e695ee295e94d065a4f1d
Your Name <kronicx-band@hotmail.com>
2022-07-17 01:11:24 +1000

    Update styles.ts

--- app/src/components/layout/Base/styles.ts
+++ app/src/components/layout/Base/styles.ts
@@ -6,7 +6,7 @@ export const useStyles = makeStyles((theme: Theme) => ({
   background: {
     width: '100vw',
     height: '100vh',
-    background: 'app/build/RaffleBG.png',
+    background: '#80c2ee',
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
     backgroundPosition: 'center',
